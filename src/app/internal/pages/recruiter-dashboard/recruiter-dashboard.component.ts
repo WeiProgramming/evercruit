823e600229ec17999c69b1aaf783333c983814d2
Wei Leung <wei.leung1995@gmail.com>
2018-10-14 22:44:29 -0700

    added in recruiter dashboard page

--- src/app/internal/pages/recruiter-dashboard/recruiter-dashboard.component.ts
+++ src/app/internal/pages/recruiter-dashboard/recruiter-dashboard.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+import {SelectionModel} from '@angular/cdk/collections';
+
+export interface PeriodicElement {
+  first: string;
+  last: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {first: 'Jordan ', last: 'Braus'},
+  {first: 'Lauren', last: 'Volkskaya'}
+];
 
 @Component({
   selector: 'app-recruiter-dashboard',
   templateUrl: './recruiter-dashboard.component.html',
   styleUrls: ['./recruiter-dashboard.component.scss']
 })
-export class RecruiterDashboardComponent implements OnInit {
+export class RecruiterDashboardComponent {
 
-  constructor() { }
+  displayedColumns: string[] = ['select', 'first', 'last'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
 
-  ngOnInit() {
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
   }
 
 }
